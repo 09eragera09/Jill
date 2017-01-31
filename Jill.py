e9c84b5a217a140e2afd81a7cc86423c6d1cde97
0eragera09 <gg873702@gmail.com>
2017-01-31 08:34:12 +0530

    Jill no longer uses Chiaki's assets

--- Jill.py
+++ Jill.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from time import time
 import logging
 from asyncio import sleep, coroutine
-from wand.drawing import Drawing
+from wand.drawing import Drawing, TEXT_ALIGN_TYPES
 from wand.image import Image
 from wand.color import Color
 import traceback
@@ -17,14 +17,10 @@ def imageGen(member):
     with Drawing() as draw:
         draw.fill_color = Color('white')
         draw.font = './cogs/assets/Whitney_Medium.ttf'
-        if 15 < len(member.name) < 22:
-            draw.font_size = 35
-        elif len(member.name) >= 22:
-            draw.font_size = 30
-        else:
-            draw.font_size = 40
-        draw.text(x=510, y=350, body="User: %s#%s" % (member.name, member.discriminator))
-        with Image(filename='./cogs/assets/KUD_3.png') as image:
+        draw.font_size = 55
+        draw.text_alignment = 'right'
+        draw.text(x=1465, y=512, body="User: %s#%s" % (member.name, member.discriminator))
+        with Image(filename='./cogs/assets/Stella_KUD.png') as image:
             draw(image)
             image.save(filename='./cogs/assets/test.png')
             return None
@@ -45,7 +41,7 @@ def calculateTime(totalseconds):
 @bot.command(name='reload', hidden=True, pass_context=True)
 @checks.is_owner()
 async def _reload(ctx, *, cogs: str = None):
-    msg1 = await bot.say('Reloading Cogs...', delete_after=10)
+    await bot.say('Reloading Cogs...', delete_after=10)
     if cogs is not None:
         cogs = cogs.split(' ')
     elif cogs is None:
@@ -70,6 +66,7 @@ async def _reload(ctx, *, cogs: str = None):
         else:
             load_cogs.append(cog.replace('py', ''))
     del temp
+    temp = []
     if len(load_cogs) != 0:
         msg_loaded = '***Cogs Reloaded:*** `{}`'.format(", ".join(load_cogs))
         temp.append(msg_loaded)
@@ -96,6 +93,8 @@ async def on_ready():
         bot.load_extension(x)
     global bot_startup
     bot_startup = getTime()
+    game = discord.Game
+    await bot.change_presence(game=game(name="VA-11 HALL-A"))
 
 @coroutine
 @bot.event
@@ -127,7 +126,7 @@ async def on_voice_state_update(before, after):
 
 @bot.event
 async def on_member_join(member):
-    imagegen = imageGen(member)
+    imageGen(member)
     await bot.send_file(member.server, './cogs/assets/test.png', content="Welcome to Kindly United Dreams, %s, Please read the rules over at <#%s>" % (member.mention, [x.id for x in member.server.channels if x.name == "readme"][0]))
     music_role = [x for x in member.server.roles if x.name == "Music"][0]
     image_role = [x for x in member.server.roles if x.name == "Image"][0]
@@ -140,29 +139,39 @@ async def on_member_join(member):
 
 @bot.command()
 async def source():
-    await bot.say("Here's my source code https://github.com/09eragera09/Jill/blob/master/Jill.py")
+    '''Get source on github'''
+    await bot.say("Here's my source code https://github.com/09eragera09/Jill")
 
 
 @bot.command()
 async def invite():
+    '''Invite link to add to more servers'''
     await bot.say("Here's my invite link https://discordapp.com/oauth2/authorize?&client_id=271241978556055552&scope=bot")
 
 
 @bot.command()
+@checks.is_owner()
 async def setGame(*, game_name: str = None):
     game = discord.Game
     await bot.change_presence(game=game(name=game_name))
 
 @bot.command()
 async def status():
+    """Get the bot status"""
     embed = discord.Embed(title="%s#%s" % (bot.user.name, bot.user.discriminator), description="A bot written in python", color=0x9A32CD)
     embed.add_field(name="Owner", value="Era#4669", inline=False)
     currentTime = getTime()
     seconds = int(currentTime - bot_startup)
     uptime = calculateTime(seconds)
-    embed.add_field(name="Uptime", value="have been mixing drinks for %sd%sh%sm%ss" % (uptime[0], uptime[1], uptime[2], uptime[3]), inline=False)
+    embed.add_field(name="Uptime", value="Have been mixing drinks for %sd%sh%sm%ss" % (uptime[0], uptime[1], uptime[2], uptime[3]), inline=False)
     embed.set_thumbnail(url=bot.user.avatar_url)
     await bot.say(embed=embed)
+
+@bot.command(name='welcome', aliases=['wleocme'], pass_context=True, hidden=True)
+async def wleocme(ctx):
+    member = ctx.message.author
+    imageGen(member)
+    await bot.send_file(member.server, './cogs/assets/test.png', content="Welcome to Kindly United Dreams, %s, Please read the rules over at <#%s>" %(member.mention, [x.id for x in member.server.channels if x.name == "readme"][0]))
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
